74865f2fba3dcc94c85548ae54a32029d56c1961
ShubhamCodeByte <shubhamsingh22401@gmail.com>
2026-04-14 01:32:10 +0530

    removed the dev code for proper program code , remove the extra comments which was for hte understanding and mistakes.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import speech_recognition as sr  # this we can use to call it using the short na
 import webbrowser as wb
 import pyttsx3
 import musiclibrary
-# import requests
 import pyjokes
 import google.generativeai as genai
 
@@ -12,14 +11,13 @@ genai.configure(api_key="AIzaSyCOwxmoiiTBfuawQUZDDf-lvRoSaN_hJlE")
 
 
 # Configure the AI to give short, voice-friendly answers
-model = genai.GenerativeModel(
-    # 'gemini-1.5-flash',     # this is the older version this is not working 
+model = genai.GenerativeModel( 
     'gemini-3-flash-preview',
     system_instruction="You are a helpful voice assistant. Keep your answers extremely brief, conversational, and to the point. Use only 1 or 2 short sentences. Do not use asterisks, bolding, or lists."
 )
 chat = model.start_chat(history=[])
 
-# function to process the command and give the out put 
+# function to process the command and give the output 
 def processGenAi(command_text):
     # sends text to Gemini and speaks the response
     try:
@@ -36,14 +34,10 @@ def giveJoke():
   joke = pyjokes.get_joke()
   return joke
 
-
-# pip install pocketsphinx ---> this we have run to make the sphinx working 
-
-
 # creating the object for the recognition 
 recognizer = sr.Recognizer()
-# engine = pyttsx3.init()   # this was running only one time therefore it is saying it only one time first one 
 
+# fuction to speak the text
 def speak(text):
   engine = pyttsx3.init()
   engine.say(text)
@@ -51,8 +45,6 @@ def speak(text):
 
 # this will process the command 
 def processCommand(c: str):
-  #  if (c.lower() == "open Jarvis"):    this we are not using we are using this in different fromat
-
   if "open google" in c.lower() :
    speak("opening google")
    wb.open("https://www.google.com/")
@@ -72,7 +64,7 @@ def processCommand(c: str):
   elif c.lower().startswith("play") :
    song = c.lower().split(" ")[1]
    speak(f"playing {song}")
-   link = musiclibrary.music[song]    # mistake : i have used the parenthesis here but this is a dictionary 
+   link = musiclibrary.music[song]   
    wb.open(link)
   elif(c.lower() == "say joke") :
    speak("here is a joke for you.")
@@ -92,14 +84,6 @@ if __name__ == "__main__" :
     # obtain audio from the microphone
     r = sr.Recognizer()
 
-    # this we moved to the try section to dont get the error
-    # with sr.Microphone() as source:
-    #     print("Listening.......")
-    #     audio = r.listen(source,timeout=2)   # this take the two argument timeout to do the recognitze faster we need to set the timeout 
-
-
-    print("Recognizing....")
-    # recognize speech using Sphinx
     try:
         # print("Sphinx thinks you said " + r.recognize_sphinx(audio))
         # command = r.recognize_sphinx(audio)  we are not using this as this is not giving the proper answer
@@ -112,14 +96,11 @@ if __name__ == "__main__" :
             r.adjust_for_ambient_noise(source, duration=0.5)
 
             audio = r.listen(source,timeout=3,phrase_time_limit=2)  # this is for listening **Timeout** = max wait time to start talking; **Phrase Limit** = max length of the recording.
+        print("Recognizing......")
         word = r.recognize_google(audio)
         print(word)
-        
-        """---------------------------------------------------------------------"""
-
-        # this is just for the demo 
-        # word = input("Please enter the command to activate.... \n:")
-        # if(word.lower() == "jarvis" or word.lower() == "google"):
+      
+        #check for the two word to invoke the command listening 
         if ("jarvis" in word.lower() or "google" in word.lower()):     # this is good as i have increased the time limit of listening 
             speak("yes how may i help you")
             # Tune the recognizer BEFORE listening
@@ -128,7 +109,6 @@ if __name__ == "__main__" :
             r.dynamic_energy_threshold = True  # Auto-adjusts to background noise
 
             # Listen for command 
-            """This is disabled only for the demo of the function tommorrow need to intialize"""
             with sr.Microphone() as source:
                 print("Jarvis Active, Listening for command....")
                 # timeout=5: Waits 5 seconds for you to START talking
@@ -141,9 +121,7 @@ if __name__ == "__main__" :
                    )
                 command = r.recognize_google(audio)
                 print(command)
-            """ Till here """
 
-            # command = input("please enter the command ")
             # we are processing the command here 
             processCommand(command)
 
@@ -151,8 +129,3 @@ if __name__ == "__main__" :
     except Exception as e :
         print("Error; {0}".format(e))
     
-    # this we are not using 
-    # except sr.UnknownValueError:
-    #     print(" could not understand audio")
-    # except sr.RequestError as e:
-    #     print("Sphinx error; {0}".format(e))
