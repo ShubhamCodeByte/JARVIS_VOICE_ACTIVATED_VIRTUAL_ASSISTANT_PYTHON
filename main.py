7d0ba3321bafb6843415eff78484ae62dedabd78
ShubhamCodeByte <shubhamsingh22401@gmail.com>
2026-04-14 01:12:25 +0530

    dynamic open website , google gemni api integration and updated to the gemni 3 model , tuning of the speech recognition

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ import google.generativeai as genai
 
 
 # google ai studio api key 
-genai.configure(api_key="AIzaSyC-mRs-k5trWLR3NefR7CC2v9ssz0CqIbA")
+genai.configure(api_key="AIzaSyCOwxmoiiTBfuawQUZDDf-lvRoSaN_hJlE")
 
 
 # Configure the AI to give short, voice-friendly answers
 model = genai.GenerativeModel(
-    'gemini-1.5-flash',
+    # 'gemini-1.5-flash',     # this is the older version this is not working 
+    'gemini-3-flash-preview',
     system_instruction="You are a helpful voice assistant. Keep your answers extremely brief, conversational, and to the point. Use only 1 or 2 short sentences. Do not use asterisks, bolding, or lists."
 )
 chat = model.start_chat(history=[])
 
-
+# function to process the command and give the out put 
 def processGenAi(command_text):
     # sends text to Gemini and speaks the response
     try:
@@ -30,7 +31,7 @@ def processGenAi(command_text):
         print(f"API Error: {e}")
         speak("Sorry, I am having trouble connecting to my brain.")
 
-
+# This is to give the random joke using the pyjoke module 
 def giveJoke():
   joke = pyjokes.get_joke()
   return joke
@@ -64,12 +65,16 @@ def processCommand(c: str):
   elif "open youtube" in c.lower() :
    speak("opening youtube")
    wb.open("https://www.youtube.com/")
+  elif c.lower().startswith("open website") :           # **this is added to open the websites dynamically 
+   website = c.lower().split(" ")[2]
+   speak(f"opening {website}")                       
+   wb.open(f"https://www.{website}.com/")
   elif c.lower().startswith("play") :
    song = c.lower().split(" ")[1]
    speak(f"playing {song}")
    link = musiclibrary.music[song]    # mistake : i have used the parenthesis here but this is a dictionary 
    wb.open(link)
-  elif "joke" in c.lower() :
+  elif(c.lower() == "say joke") :
    speak("here is a joke for you.")
    speak(giveJoke())
   else :
@@ -102,23 +107,40 @@ if __name__ == "__main__" :
         """ This is just for the silent demo tommorow we will initialize it """
         with sr.Microphone() as source:
             print("Listening.......")
-            audio = r.listen(source,timeout=2,phrase_time_limit=1)  # this is for listening one word only
-        word = r.recognize_Jarvis(audio)
+       
+            # 1. This is the magic line that fixes inconsistency!
+            r.adjust_for_ambient_noise(source, duration=0.5)
+
+            audio = r.listen(source,timeout=3,phrase_time_limit=2)  # this is for listening **Timeout** = max wait time to start talking; **Phrase Limit** = max length of the recording.
+        word = r.recognize_google(audio)
         print(word)
         
         """---------------------------------------------------------------------"""
 
         # this is just for the demo 
         # word = input("Please enter the command to activate.... \n:")
-        if(word.lower() == "Jarvis"):
-            speak("yes How may i help you")
+        # if(word.lower() == "jarvis" or word.lower() == "google"):
+        if ("jarvis" in word.lower() or "google" in word.lower()):     # this is good as i have increased the time limit of listening 
+            speak("yes how may i help you")
+            # Tune the recognizer BEFORE listening
+            r.pause_threshold = 1.5        # Wait 2 seconds of silence before stopping (default 0.8)
+            r.energy_threshold = 300       # Mic sensitivity (adjust if noisy environment)
+            r.dynamic_energy_threshold = True  # Auto-adjusts to background noise
 
             # Listen for command 
             """This is disabled only for the demo of the function tommorrow need to intialize"""
             with sr.Microphone() as source:
                 print("Jarvis Active, Listening for command....")
-                audio = r.listen(source)
-                command = r.recognize_Jarvis(audio)
+                # timeout=5: Waits 5 seconds for you to START talking
+                # phrase_time_limit=12: Stops recording after 12 seconds total 
+                # This keeps the Gemini input short and prevents token exhaustion
+                audio = r.listen(
+                   source,
+                   timeout=5,
+                   phrase_time_limit=33, # this will auto stop as the threshold is set 
+                   )
+                command = r.recognize_google(audio)
+                print(command)
             """ Till here """
 
             # command = input("please enter the command ")
